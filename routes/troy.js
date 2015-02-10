5e92b04aaec63e73733c0fc4cde2c88aeaeaef69
Troy Connor <troy.connor@yahoo.com>
2015-02-09 19:23:13 -0500

    Added req.params.id to troy route.

--- routes/troy.js
+++ routes/troy.js
@@ -5,6 +5,9 @@ router.get('/',function(req,res) {
 	res.send("This is the Troy route.");
 	}
 );
-
+router.get('/:id', function(req, res) { 
+	res.send("This is #" + req.params.id + " try");	
+	}
+); 
 module.exports = router;
 
