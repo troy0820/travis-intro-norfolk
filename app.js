163d365c9cf1958ea9364c684fde4157a2bcaf6f
Troy Connor <troy.connor@yahoo.com>
2015-02-09 19:18:52 -0500

    Added troy route.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var troy = require('./routes/troy');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/troy', troy);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
