4f91a9dfbfcef833efdbf7fb8969f8834b878e6b
Troy Connor <troy.connor@yahoo.com>
2015-02-12 19:44:36 -0500

    Added view for troy/:id route.

--- routes/troy.js
+++ routes/troy.js
@@ -5,8 +5,10 @@ router.get('/',function(req,res) {
 	res.render('troy',{ name: 'Troy Connor', title: 'Troy\'s Route page' });
 	}
 );
+
 router.get('/:id', function(req, res) { 
-	res.send("This is #" + req.params.id + " try");	
+	var id = req.params.id;
+	res.render('troy',{ name: 'Troy Connor', title: 'Troy\'s id page', id:id });	
 	}
 ); 
 module.exports = router;
