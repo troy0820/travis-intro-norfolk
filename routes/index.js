c43cffe48bd9918a79269f2da69d7c2ece2ac7f4
Troy Connor <troy.connor@yahoo.com>
2015-03-03 21:47:43 -0800

    Added some stuff to the route and view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'troy0280 page' });
+  	var num = Math.floor((Math.random() *100) +1 );
+        var quote = 'To avoid criticism say nothing, do nothing, be nothing. - Aristotle';
+	res.render('index', { 
+	title: 'Welcome to ', 
+	name: 'Troy\'s New App', 
+	num:num, 
+	quote:quote
+	 });
 });
 
 module.exports = router;
