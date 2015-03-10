a449bf6ce973fc398543948329c4f3e68a24b59b
Troy Connor <troy.connor@yahoo.com>
2015-03-10 18:05:25 -0400

    Added array for post route 'names'

--- routes/troy.js
+++ routes/troy.js
@@ -5,7 +5,8 @@ var names = [];
 
 router.get('/',function(req,res) {
 	var num = Math.floor((Math.random() * 100) +1);
-	res.render('troy',{ name: 'Troy Connor', title: 'Troy\'s Route page', num:num });
+	res.render('troy',{ name: 'Troy Connor', title: 'Troy\'s Route page', num:num, names:names });
+	//console.log(people);
 	}
 );
 
@@ -20,7 +21,9 @@ router.post('/', function(req,res, next) {
 	var name = req.body.troy;
 	var num = Math.floor((Math.random() * 100) +1);
 	var id = 'This is the new ID for the Posted route';
-	res.render('troy',{ name:name, title: 'Posted from the post route', id:'New page', num:num });
+	names.push(name);
+	res.render('troy',{ name:name, title: 'Posted from the post route', id:'New page', num:num, names:names });
+
 	
 });
 
