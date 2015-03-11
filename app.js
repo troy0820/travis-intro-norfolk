4e990c4f6dacba450c10237b4c2a1e97ac5de146
Troy Connor <troy.connor@yahoo.com>
2015-03-11 18:22:52 -0400

    Deleted user route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var troy = require('./routes/troy');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 
 var app = express();
@@ -23,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/troy', troy);
 
 /// catch 404 and forward to error handler
