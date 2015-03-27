a1f4e9b6849f5f3be67069bc227e3dd71b8e0e71
Troy Connor <troy.connor@yahoo.com>
2015-03-26 22:13:26 -0400

    Added font awesome with icon on index

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
