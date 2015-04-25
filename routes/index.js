55d084810f1e2ad42b1a39292b209a9ac5d22598
Troy Connor <troy.connor@yahoo.com>
2015-04-25 16:45:19 -0400

    Format

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   	var num = Math.floor((Math.random() *100) +1 );
-        var quote = 'To avoid criticism say nothing, do nothing, be nothing. - Aristotle';
+    var quote = 'To avoid criticism say nothing, do nothing, be nothing. - Aristotle';
 	res.render('index', { 
 	title: 'Welcome to ', 
 	name: 'Troy\'s New App', 
