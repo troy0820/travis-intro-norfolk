ce06147a0a3e196c65488cbfdfbd464d04dd2080
Troy Connor <troy.connor@yahoo.com>
2015-05-16 13:27:47 -0400

    Formatted again

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   	var num = Math.floor((Math.random() *100) +1 );
-    var quote = 'To avoid criticism say nothing, do nothing, be nothing. - Aristotle';
+        var quote = 'To avoid criticism say nothing, do nothing, be nothing. - Aristotle';
 	res.render('index', { 
-	title: 'Welcome to ', 
-	name: 'Troy\'s New App', 
-	num:num, 
-	quote:quote
-	 });
+		title: 'Welcome to ', 
+		name: 'Troy\'s New App', 
+		num:num, 
+		quote:quote
+		 });
 });
 
 module.exports = router;
